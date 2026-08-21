9d4e76e45c561f2d0b9b421704ede6ab86b37c48
Manohar <manohar.x03@gmail.com>
2026-08-21 20:48:31 +0530

    Binary Search

---
+++ Arrays/Basics/BinarySearch.java
@@ -0,0 +1,29 @@
+package Arrays.Basics;
+
+public class BinarySearch {
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 6, 7, 8, 9 };
+
+        System.out.println(binarySearch(arr, 7));
+    }
+}
